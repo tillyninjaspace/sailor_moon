70d4a21023d15c4ad91372e98e0d7d8f25903f66
tillyninjaspace <squealingpet@gmail.com>
2021-06-23 17:18:35 -0700

    fix found function

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -62,7 +62,7 @@ const WishList = () => {
         <>
             <h2>My Wishlist</h2>
             < GuestWishes/>
-            <h4 style={{textAlign: "center", margin: "5px"}}><a href="https://www.amazon.com/s?k=sailor+moon+merchandise&amp;ref=nb_sb_noss_2&_encoding=UTF8&tag=sailormoon00e-20&linkCode=ur2&linkId=d645d325aab67aeb13bceff654c6dc40&camp=1789&creative=9325" target="_blank">Find Sailor Moon Merchandise on Amazon</a></h4>
+            <h5 style={{textAlign: "center", margin: "5px"}}><a href="https://www.amazon.com/s?k=sailor+moon+merchandise&amp;ref=nb_sb_noss_2&_encoding=UTF8&tag=sailormoon00e-20&linkCode=ur2&linkId=d645d325aab67aeb13bceff654c6dc40&camp=1789&creative=9325" target="_blank">Find Sailor Moon Merchandise on Amazon</a></h5>
             <div style={{border: "5px solid white", padding: "1em"}} className="pickListWrapper">
              { sailorMoonItems.results && 
                 sailorMoonItems.results.map((star) => 
@@ -77,21 +77,42 @@ const WishList = () => {
                     <form className="addForm"
                         onSubmit={ (event) => {
                             event.preventDefault();
-                            setWishList([...wishList, newWish])
-                            setOpenModal(true)
-                        }}>
-                        <button
-                            onClick={(event) => {
+                            //new
                             const found = wishList.find(existing => {
-                                console.log(wishList)
                                 if (existing == star.title){
                                     return true
                                 }
-                                    console.log("hello, looking", existing,
-                                    "star.title:", star.title)
                                 })
-                                console.log("Did I find it? ", found)
-                                setNewWish(star.title)
+
+                            //end new
+
+                            if (!found) {
+                            setWishList([...wishList, newWish])
+                            setOpenModal(true)
+                            } else {
+                                alert('You have already added this.')
+                              return;
+                            }
+                            
+                        }}>
+                        <button
+                            onClick={(event) => {
+                            // const found = wishList.find(existing => {
+                            //     console.log(wishList)
+                            //     if (existing == star.title){
+                            //         return true
+                            //     }
+                            //         console.log("hello, looking", existing,
+                            //         "star.title:", star.title)
+                            //     })
+                            //     console.log("Did I find it? ", found)
+
+                                // if (found) {
+                                //     return;
+                                // } else {
+                                    setNewWish(star.title)
+                                // }
+                                
                           
                                 
                             // )
